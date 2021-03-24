be2259fea103a68e3834f22e143e3c58f75fad04
Kovalev Pavel <kovalev5690@gmail.com>
2021-03-24 17:10:21 +0300

    Tidy winner_definer

--- pkg/winner-definer/winner_definer.go
+++ pkg/winner-definer/winner_definer.go
@@ -12,49 +12,32 @@ type PlayerChoice struct {
 }
 
 func (wd *WinnerDefiner) GetWinners(playersChoices []PlayerChoice) []string {
-	// => [
-	// 	{
-	// 		id: "Vasya",
-	// 		choice: domain.PAPER,
-	// 	},
-	// 	{
-	// 		id: "Petya",
-	// 		choice: domain.PAPER,
-	// 	},
-	// 	{
-	// 		id: "Masha",
-	// 		choice: domain.ROCK,
-	// 	},
-	// ]
-	// max(...) => the strongest choice
-	// => ["Vasya", "Petya"]
-	count := [3]int{0}
+	const numberOfChoices = 3
+	count := [numberOfChoices]int{0}
 	for _, choice := range playersChoices {
 		count[int(choice.Input)]++
 	}
 	missing := -1
 	missingKol := 0
-	for i := 0; i < 3; i++ {
+	for i := 0; i < numberOfChoices; i++ {
 		if count[i] == 0 {
 			missing = i
 			missingKol++
 		}
 	}
-	if missingKol > 1 {
+	if missingKol != 1 {
 		return nil
 	}
-	ret := []string{}
 	var another domain.Choice
 	switch missing {
-	case -1: // draw, all players lost
-		return nil
 	case 0:
 		another = domain.SCISSORS
 	case 1:
-		another = domain.SCISSORS
+		another = domain.ROCK
 	case 2:
 		another = domain.PAPER
 	}
+	ret := []string{}
 	for _, choice := range playersChoices {
 		if choice.Input.Compare(another) == 0 {
 			ret = append(ret, choice.PlayerID)
