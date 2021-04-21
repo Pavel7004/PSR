78d2395652b6748e671792638778594e48c3fbbf
Kovalev Pavel <kovalev5690@gmail.com>
2021-04-21 17:53:11 +0300

    Added timeout to sending message to subscriber

--- pkg/subscribe/subscribe.go
+++ pkg/subscribe/subscribe.go
@@ -1,6 +1,10 @@
 package subscribe
 
-import "errors"
+import (
+	"errors"
+	"github.com/rs/zerolog/log"
+	"time"
+)
 
 var (
 	ErrTopicNotPresent = errors.New("Topic not found")
@@ -27,13 +31,28 @@ func (p *Publisher) Publish(topic string, msg interface{}) error {
 	if !p.HasTopic(topic) {
 		return ErrTopicNotPresent
 	}
+	send := make(chan interface{})
+	ok := make(chan struct{})
+	go func() {
+		msg := <-send
+		ok <- struct{}{}
+	}()
+	count := 0
 	for _, sub := range p.topics[topic] {
-		sub <- msg
+		timer := time.NewTimer(45 * time.Second)
+		select {
+		case <-timer.C:
+			count++
+		case <-ok:
+		}
+	}
+	if count != 0 {
+		log.Info().Msgf("Not every sub on topic \"%v\" accepted msg = %v", topic, msg)
 	}
 	return nil
 }
 
-func (p *Publisher) Subsciribe(sub Subscriber, topic string) error {
+func (p *Publisher) Subscribe(sub Subscriber, topic string) error {
 	if p.HasTopic(topic) {
 		p.topics[topic] = append(p.topics[topic], sub)
 	} else {
@@ -41,20 +60,3 @@ func (p *Publisher) Subsciribe(sub Subscriber, topic string) error {
 	}
 	return nil
 }
-
-// package main
-
-// import (
-// 	"fmt"
-// 	"sync"
-// )
-
-// func main() {
-// 	var once sync.Once
-
-// 	for i := 0; i < 1000; i++ {
-// 		once.Do(func() {
-// 			fmt.Println(" *Executed *")
-// 		})
-// 	}
-// }
