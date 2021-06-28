a4892aec619595648a9fa7c38c5e1f9c57c3c3c7
Kovalev Pavel <kovalev5690@gmail.com>
2021-06-28 15:08:23 +0300

    Added score count to player

--- pkg/domain/player.go
+++ pkg/domain/player.go
@@ -1,15 +1,29 @@
 package domain
 
 type Player struct {
-	ID 		string
+	id    string
+	score int
 }
 
 func NewPlayer(id string) *Player {
 	return &Player{
-		ID: id,
+		id:    id,
+		score: 0,
 	}
 }
 
 func (p *Player) GetID() string {
-	return p.ID
+	return p.id
+}
+
+func (p *Player) GetScore() int {
+	return p.score
+}
+
+func (p *Player) IncrementScore() {
+	p.score++
+}
+
+func (p *Player) ResetScore() {
+	p.score = 0
 }
