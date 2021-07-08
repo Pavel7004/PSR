ce98fdbebc55160ca630175a679c9a4b4768a1d7
Kovalev Pavel <kovalev5690@gmail.com>
2021-07-08 17:53:29 +0300

    Finish base score_manager structure

--- pkg/score-manager/score_manager.go
+++ pkg/score-manager/score_manager.go
@@ -1,5 +1,31 @@
 package scoremanager
 
+import "github.com/pavel/PSR/pkg/domain"
+
 type ScoreManager struct {
 	playersScores map[string]int
+}
+
+func NewScoreManager(players []*domain.Player) *ScoreManager {
+	newScores := make(map[string]int, len(players))
+	for _, name := range players {
+		newScores[name.GetID()] = 0
+	}
+	return &ScoreManager{
+		playersScores: newScores,
+	}
+}
+
+func (sm *ScoreManager) GetPlayerScore(name string) int {
+	return sm.playersScores[name]
+}
+
+func (sm *ScoreManager) IncrementPlayerScore(name string) {
+	sm.playersScores[name]++
+}
+
+func (sm *ScoreManager) ResetPlayersScores() {
+	for name := range sm.playersScores {
+		sm.playersScores[name] = 0
+	}
 }
