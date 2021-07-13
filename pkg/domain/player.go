f66999a63831aee3f909ae3486155421c54da5cd
Kovalev Pavel <kovalev5690@gmail.com>
2021-07-13 15:53:07 +1000

    Remove score tracking logic from player.go

--- pkg/domain/player.go
+++ pkg/domain/player.go
@@ -1,29 +1,15 @@
 package domain
 
 type Player struct {
-	id    string
-	score int
+	id string
 }
 
 func NewPlayer(id string) *Player {
 	return &Player{
-		id:    id,
-		score: 0,
+		id: id,
 	}
 }
 
 func (p *Player) GetID() string {
 	return p.id
-}
-
-func (p *Player) GetScore() int {
-	return p.score
-}
-
-func (p *Player) IncrementScore() {
-	p.score++
-}
-
-func (p *Player) ResetScore() {
-	p.score = 0
 }
