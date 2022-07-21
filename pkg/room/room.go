801ca4450065f41dc5fa2bd54f3ffcc591afd5f1
Kovalev Pavel <kovalev5690@gmail.com>
2022-07-21 15:38:18 +0300

    Remove connToPlayer field in room

--- pkg/room/room.go
+++ pkg/room/room.go
@@ -17,7 +17,6 @@ type Room struct {
 	game               *game.Game
 	cfg                *RoomConfig
 	mtx                *sync.Mutex
-	connectionToPlayer map[*websocket.Conn]string
 	playerToConnection map[string]*websocket.Conn
 	roomStateSub       *subscribe.Subscriber
 	winnersSub         *subscribe.Subscriber
@@ -41,7 +40,6 @@ func NewRoom(cfg *RoomConfig) *Room {
 	return &Room{
 		game:               game,
 		mtx:                new(sync.Mutex),
-		connectionToPlayer: make(map[*websocket.Conn]string, cfg.MaxPlayerCount),
 		playerToConnection: make(map[string]*websocket.Conn, cfg.MaxPlayerCount),
 		roomStateSub:       subRoomState,
 		winnersSub:         subWinners,
@@ -76,7 +74,7 @@ func (r *Room) RoundProcess() {
 			log.Error().Err(err).Msgf("Incrementing score for player \"%s\" error", name)
 		}
 	}
-	for conn, name := range r.connectionToPlayer {
+	for name, conn := range r.playerToConnection {
 		if err := conn.WriteMessage(websocket.TextMessage, []byte(messages[getMessage(name)])); err != nil {
 			log.Error().Err(err).Msgf("Error sending winner signal to player \"%s\"", name)
 		}
@@ -86,7 +84,7 @@ func (r *Room) RoundProcess() {
 func (r *Room) Main() {
 	r.roomStateSub.Receive()
 	startMsg := []byte("Игра началась")
-	for conn := range r.connectionToPlayer {
+	for _, conn := range r.playerToConnection {
 		if err := conn.WriteMessage(websocket.TextMessage, startMsg); err != nil {
 			log.Error().Err(err).Msgf("Error sending message \"%s\"", startMsg)
 		}
@@ -111,7 +109,7 @@ func (r *Room) Main() {
 }
 
 func (r *Room) CloseConnections() {
-	for conn, id := range r.connectionToPlayer {
+	for id, conn := range r.playerToConnection {
 		if err := conn.Close(); err != nil {
 			log.Warn().Err(err).Msgf("Player \"%s\": closing connection error", id)
 		}
@@ -125,24 +123,23 @@ func (r *Room) AddPlayer(id string, conn *websocket.Conn) {
 		return
 	}
 	r.mtx.Lock()
-	r.connectionToPlayer[conn] = id
 	r.playerToConnection[id] = conn
 	r.mtx.Unlock()
 	log.Info().Msgf("Player %s: connection established", id)
-	go r.listenConn(conn)
+	go r.listenConn(id, conn)
 }
 
-func (r *Room) listenConn(conn *websocket.Conn) {
+func (r *Room) listenConn(id string, conn *websocket.Conn) {
 	for {
 		tMsg, msg, err := conn.ReadMessage()
 		if websocket.IsCloseError(err, websocket.CloseNormalClosure) {
 			break
 		}
 		if err != nil {
-			log.Warn().Err(err).Msgf("Reading message from %s error", r.connectionToPlayer[conn])
+			log.Warn().Err(err).Msgf("Reading message from %s error", id)
 			err = conn.Close()
 			if err != nil {
-				log.Warn().Err(err).Msgf("Error closing connection for player %s", r.connectionToPlayer[conn])
+				log.Warn().Err(err).Msgf("Error closing connection for player %s", id)
 			}
 			break
 		}
@@ -150,14 +147,14 @@ func (r *Room) listenConn(conn *websocket.Conn) {
 			log.Warn().Msgf("Message type isn't text, got = %v, expected = %v", tMsg, websocket.TextMessage)
 			continue
 		}
-		log.Info().Msgf("Got message from %s: %v", r.connectionToPlayer[conn], string(msg))
+		log.Info().Msgf("Got message from %s: %v", id, string(msg))
 		choice, err := domain.GetChoiceByName(string(msg))
 		if err != nil {
-			log.Warn().Err(err).Msgf("Player %s: invalid choice %v", r.connectionToPlayer[conn], string(msg))
+			log.Warn().Err(err).Msgf("Player %s: invalid choice %v", id, string(msg))
 			continue
 		}
 		r.game.Choose(&winner_definer.PlayerChoice{
-			PlayerID: r.connectionToPlayer[conn],
+			PlayerID: id,
 			Input:    choice,
 		})
 	}
