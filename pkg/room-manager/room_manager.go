a137549dbbee90ac3d3ebd977a3f00adad69b88f
Kovalev Pavel <kovalev5690@gmail.com>
2022-07-21 17:24:57 +0300

    Implement RoomManager

--- pkg/room-manager/room_manager.go
+++ pkg/room-manager/room_manager.go
@@ -5,11 +5,14 @@ import (
 	"sync"
 
 	"github.com/pavel/PSR/pkg/room"
-	"github.com/rs/zerolog/log"
 )
 
 var (
-	ErrRoomAlreadyExists = errors.New("Room with this name already exists.")
+	ErrRoomAlreadyExists  = errors.New("Room with this name already exists.")
+	ErrRoomDontExist      = errors.New("Room with this name aren't present.")
+	ErrInvalidPlayerCount = errors.New("Room can't have less then 1 player.")
+	ErrInvalidMaxScore    = errors.New("Room score must be more than 0.")
+	ErrTimeoutTooShort    = errors.New("Room round time must be more than 5 seconds.")
 )
 
 type RoomManager struct {
@@ -25,15 +28,42 @@ func New() *RoomManager {
 }
 
 func (rm *RoomManager) CreateRoom(cfg *room.RoomConfig) error {
-	rm.mtx.Lock()
-	if _, exist := rm.rooms[cfg.Name]; exist {
-		rm.mtx.Unlock()
-		log.Warn().Msgf("RoomManager: room with name %s already exists", cfg.Name)
-
-		return ErrRoomAlreadyExists
+	err := rm.CheckRoomConfig(cfg)
+	if err != nil {
+		return err
 	}
+
+	rm.mtx.Lock()
 	rm.rooms[cfg.Name] = room.NewRoom(cfg)
 	rm.mtx.Unlock()
 
 	return nil
+}
+
+func (rm *RoomManager) CheckRoomConfig(cfg *room.RoomConfig) error {
+	rm.mtx.Lock()
+	_, exist := rm.rooms[cfg.Name]
+	rm.mtx.Unlock()
+	if exist {
+		return ErrRoomAlreadyExists
+	}
+	if cfg.MaxPlayerCount < 1 {
+		return ErrInvalidPlayerCount
+	}
+	if cfg.MaxScore < 1 {
+		return ErrInvalidMaxScore
+	}
+	if cfg.RoundTimeout.Seconds() < 5.0 {
+		return ErrTimeoutTooShort
+	}
+	return nil
+}
+
+func (rm *RoomManager) GetRoomByName(name string) (*room.Room, error) {
+	room, exist := rm.rooms[name]
+	if !exist {
+		return nil, ErrRoomDontExist
+	}
+
+	return room, nil
 }
